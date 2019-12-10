aec76c664af904b8c244d6f0d9262ff6478a6841
Sylvain Desvé <sylvain.desve@gmail.com>
2019-12-10 20:22:19 +0100

    Fixed GetAllTodoListsQuery handler.

--- src/todo/query/TodoListQueryHandler.ts
+++ src/todo/query/TodoListQueryHandler.ts
@@ -1,18 +1,15 @@
 import { Observable } from "rxjs";
 import { HandleQuery } from "ts-eventsourcing/QueryHandling/HandleQuery";
 import { QueryHandler } from "ts-eventsourcing/QueryHandling/QueryHandler";
-import {
-  GetAllTodoLists,
-  TodoListReadModel,
-  TodoListReadModelRepository
-} from "..";
+import { Repository } from "ts-eventsourcing/ReadModel/Repository";
+import { GetAllTodoLists, TodoListReadModel } from "..";
 
 export class TodoListQueryHandler implements QueryHandler {
-  constructor(private readonly repository: TodoListReadModelRepository) {}
+  constructor(private readonly repository: Repository<TodoListReadModel>) {}
 
   @HandleQuery
   public getAllTodoLists(
-    query: GetAllTodoLists
+    _query: GetAllTodoLists
   ): Observable<TodoListReadModel> {
     return this.repository.findAll();
   }
