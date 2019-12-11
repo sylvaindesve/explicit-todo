791b702c44e4936002c8605d46ba764c4063df92
Sylvain Desvé <sylvain.desve@gmail.com>
2019-12-11 11:13:23 +0100

    Added test coverage report.

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,25 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  collectCoverage: false,
+  testEnvironment: "node",
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.{ts}", "!**/*.spec.{ts}"],
+  coverageReporters: ["json-summary", "text", "lcov"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 0
+    }
+  },
   reporters: [
     "default",
-    ["./node_modules/jest-html-reporter", {
-        "pageTitle": "Test Report"
-    }]
+    [
+      "./node_modules/jest-html-reporter",
+      {
+        pageTitle: "Test Report"
+      }
+    ]
   ]
 };
